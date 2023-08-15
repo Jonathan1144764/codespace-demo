e4bf66e476d943021be86e32222c2dbac07b2cf6
Jonathan1144764 <118191483+Jonathan1144764@users.noreply.github.com>
2023-08-15 20:03:51 +0000

    something works for the first time

---
+++ node_quickstart/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+const uri = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true";
+
+app.post('/', (req, res) => {
+  console.log(req)
+  res.send('Hello World!');
+  // const client = new MongoClient(uri);
+
+  // async function run() {
+  //   try {
+  //     const database = client.db('mongodemo');
+  //     const student = database.collection('student');
+
+  //     // Query for a movie that has the title 'Back to the Future'
+  //     const query = [{ name: 'Charisse', age: 50000 }, {name: "Maryam", age: 100000}];
+  //     const result = await student.insertMany(query);
+
+  //     console.log(result);
+  //   } finally {
+  //     // Ensures that the client will close when you finish/error
+  //     await client.close();
+  //   }
+  // }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// run().catch(console.dir);
